7986010dafa77f7fc4ab4a75a3ff1e5232d3035e
Russell Brabers <brabers.russell@gmail.com>
2020-12-17 14:07:22 -0500

    Weather Manager
    - Created WeatherManager and WeatherSystem scripts
    - Created object heirarchy in scene for the scripts

---
+++ Assets/WeatherSystem/Scripts/WeatherManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[System.Serializable]
+public enum WeatherType
+{
+    SUNNY,
+    OVERCAST,
+    RAINING,
+    THUNDERSTORM
+}
+
+public class WeatherManager : MonoBehaviour
+{
+    [Header("Light Sources")]
+    public Light2D globalLight;
+    public Light2D playerLight;
+
+    [Header("Weather Controls")]
+    public WeatherType wType;
+    public float changeTime;
+    public float timer;
+
+    [Header("Weather Objects")]
+    public GameObject rainSystem;
+    public GameObject stormSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wType = WeatherType.SUNNY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
